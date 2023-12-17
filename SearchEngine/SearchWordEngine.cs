28eb46923bfd1d3710718634b7bb23db816542b7
Yael Fox <yaelf@sepiocyber.com>
2023-12-17 12:07:34 +0200

    Add project files.

---
+++ SearchEngine/SearchWordEngine.cs
@@ -0,0 +1,85 @@
+﻿using Serilog;
+
+namespace SearchEngine
+{
+    public class SearchWordEngine
+    {
+        private Dictionary<string, HashSet<int>> indexDictionary;
+        private List<string> sentences;
+        private ILogger logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SearchWordEngine"/> class.
+        /// </summary>
+        /// <param name="dataset">The dataset of sentences.</param>
+        /// <param name="logger">The logger for recording log messages.</param>
+        public SearchWordEngine(IEnumerable<string> dataset, ILogger logger)
+        {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            CreateIndexDictionary(dataset);
+        }
+
+        /// <summary>
+        /// Creates the index dictionary based on the provided dataset.
+        /// </summary>
+        /// <param name="input">The dataset of sentences.</param>
+        public void CreateIndexDictionary(IEnumerable<string> input)
+        {
+            if (input != null)
+            {
+
+                indexDictionary = new Dictionary<string, HashSet<int>>();
+                sentences = new List<string>();
+
+                for (int i = 0; i < input.Count(); i++)
+                {
+                    sentences.Add(input.ElementAt(i));
+                    string[] words = input.ElementAt(i).Split();
+
+                    foreach (string word in words)
+                    {
+                        if (!indexDictionary.ContainsKey(word))
+                        {
+                            indexDictionary[word] = new HashSet<int>();
+                        }
+
+                        indexDictionary[word].Add(i);
+                    }
+                }
+
+                logger.Information($"Index initialized with {sentences.Count} sentences and {indexDictionary.Count} unique words.");
+            }
+            else
+            {
+                logger.Warning($"Input is null.");
+            }
+        }
+
+            /// <summary>
+            /// Searches for sentences containing the specified word.
+            /// </summary>
+            /// <param name="word">The word to search for.</param>
+            /// <returns>
+            /// A list of sentences containing the specified word. 
+            /// If the word is not found, an empty list is returned.
+            /// </returns>
+            public List<string> Search(string word)
+            {
+            if (indexDictionary != null && indexDictionary.TryGetValue(word, out var result))
+            {
+                List<string> res = new List<string>();
+                foreach (int i in result)
+                {
+                    res.Add(sentences[i]);
+                }
+                return res;
+            }
+            else
+            {
+                logger.Information("Word was not found in any sentence");
+                return new List<string>();
+            }
+        }
+    }
+
+}
---
+++ SearchEngine/Tests/SearchWordEngineShould.cs
@@ -0,0 +1,121 @@
+﻿using Moq;
+using NUnit.Framework;
+using Serilog;
+
+
+namespace SearchEngine.Tests
+{
+    [TestFixture]
+    public class SearchWordEngineShould
+    {
+        [Test]
+        public void SearchEngine_ReturnsCorrectResults()
+        {
+            // Arrange
+            string[] dataset = { "Today is Sunday", "Today is not Monday" };
+
+            // Create a mock logger
+            var mockLogger = new Mock<ILogger>();
+
+            // Action
+            var searchEngine = new SearchWordEngine(dataset, mockLogger.Object);
+            List<string> resultToday = searchEngine.Search("Today");
+            List<string> resultSunday = searchEngine.Search("Sunday");
+            List<string> resultTuesday = searchEngine.Search("Tuesday");
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(resultToday, Is.EquivalentTo(new[] { "Today is Sunday", "Today is not Monday" }));
+                Assert.That(resultSunday, Is.EquivalentTo(new[] { "Today is Sunday" }));
+                Assert.That(resultTuesday, Is.Empty);
+            });
+        }
+
+        [Test]
+        public void SearchEngine_ReturnsCorrectResults2()
+        {
+            // Arrange
+            string[] dataset = { "My name is Yael", "My name is John", "My cat drinks milk" };
+
+            // Create a mock logger
+            var mockLogger = new Mock<ILogger>();
+
+            // Action
+            var searchEngine = new SearchWordEngine(dataset, mockLogger.Object);
+            List<string> myResult = searchEngine.Search("My");
+            List<string> nameResult = searchEngine.Search("name");
+            List<string> catResult = searchEngine.Search("cat");
+            List<string> dogResult = searchEngine.Search("dog");
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(myResult, Is.EquivalentTo(new[] { "My name is Yael", "My name is John", "My cat drinks milk" }));
+                Assert.That(nameResult, Is.EquivalentTo(new[] { "My name is Yael", "My name is John" }));
+                Assert.That(catResult, Is.EquivalentTo(new[] { "My cat drinks milk" }));
+                Assert.That(dogResult, Is.Empty);
+            });
+        }
+
+        [Test]
+        public void SearchEngine_SameWordTwice_ReturnsCorrectResults()
+        {
+            // Arrange
+            string[] dataset = { "My name is name", "My name is John", "My cat drinks milk" };
+
+            // Create a mock logger
+            var mockLogger = new Mock<ILogger>();
+
+            // Action
+            var searchEngine = new SearchWordEngine(dataset, mockLogger.Object);
+            List<string> myResult = searchEngine.Search("My");
+            List<string> nameResult = searchEngine.Search("name");
+            List<string> catResult = searchEngine.Search("cat");
+            List<string> dogResult = searchEngine.Search("dog");
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(myResult, Is.EquivalentTo(new[] { "My name is name", "My name is John", "My cat drinks milk" }));
+                Assert.That(nameResult, Is.EquivalentTo(new[] { "My name is name", "My name is John" }));
+                Assert.That(catResult, Is.EquivalentTo(new[] { "My cat drinks milk" }));
+                Assert.That(dogResult, Is.Empty);
+            });
+        }
+
+        [Test]
+        public void SearchEngine_EmptyDataSet_ReturnsCorrectResults()
+        {
+            // Arrange
+            string[] dataset = { };
+
+            // Create a mock logger
+            var mockLogger = new Mock<ILogger>();
+
+            // Action
+            var searchEngine = new SearchWordEngine(dataset, mockLogger.Object);
+            List<string> myResult = searchEngine.Search("My");
+
+            // Assert
+            Assert.That(myResult, Is.Empty);
+        }
+
+        [Test]
+        public void SearchEngine_NullDataSet_ReturnsCorrectResults()
+        {
+            // Arrange
+            string[] dataset = null;
+
+            // Create a mock logger
+            var mockLogger = new Mock<ILogger>();
+
+            // Action
+            var searchEngine = new SearchWordEngine(dataset, mockLogger.Object);
+            List<string> myResult = searchEngine.Search("My");
+
+            // Assert
+            Assert.That(myResult, Is.Empty);
+        }
+    }
+}
